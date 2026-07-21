22f164c23938fe3f287cf608b21fa090c80ff810
Alozie Miracle Patrick <nacotech09@gmail.com>
2026-07-21 17:19:05 +0100

    checking python types

---
+++ python_types.py
@@ -0,0 +1,14 @@
+
+
+# checking the type of a variable
+width = 15
+print(type(width))  # Output: <class 'int'>
+
+name = "Alice"
+print(type(name))  # Output: <class 'str'>
+
+length = 20.5
+print(type(length))  # Output: <class 'float'>
+
+# checking the type of a function
+print(type(print))  # Output: <class 'builtin_function_or_method'>
