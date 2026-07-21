726a5d1abed7d1e30495afeda2d5d470379aefcf
Alozie Miracle Patrick <nacotech09@gmail.com>
2026-07-21 18:41:55 +0100

    modified the code

--- 8. third_project_fridge.py
+++ 8. third_project_fridge.py
@@ -14,11 +14,45 @@ conditions:
 
 temperature = float(input("Enter the temperature in Celsius: "))
 
+# if temperature < 0:
+#     print("Fridge too cold")
+# elif 0 <= temperature <= 4:
+#     print("Fridge ok")
+# elif 4 < temperature <= 6:
+#     print("Fridge too warm")
+# else:
+#     print("Fridge broken")
+
+
+# improving code readability by using a function
+
+# status = "Fridge broken"
+
+# if temperature < 0:
+#     status = "Fridge too cold"
+# elif 0 <= temperature <= 4:
+#     status = "Fridge ok"
+# elif 4 < temperature <= 6:
+#     status = "Fridge too warm"
+# else:
+#     status = "Fridge broken"
+
+# print(status)
+
+# better improvement
+STATUS_COLD = "Fridge too cold"
+STATUS_OK = "Fridge ok"
+STATUS_WARM = "Fridge too warm"
+STATUS_BROKEN = "Fridge broken"
+
+status = STATUS_BROKEN
+
 if temperature < 0:
-    print("Fridge too cold")
+    status = STATUS_COLD
 elif 0 <= temperature <= 4:
-    print("Fridge ok")
+    status = STATUS_OK
 elif 4 < temperature <= 6:
-    print("Fridge too warm")
-else:
-    print("Fridge broken")
+    status = STATUS_WARM
+
+
+print(status)
