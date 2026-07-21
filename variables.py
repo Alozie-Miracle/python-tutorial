9595a8e11a5b3116e9e09d48c69a1a5dd025f2b0
Alozie Miracle Patrick <nacotech09@gmail.com>
2026-07-21 17:44:13 +0100

    added more variables

--- variables.py
+++ variables.py
@@ -2,3 +2,15 @@ greeting = "Hello"
 name = "Alice"
 
 print(greeting + ", " + name + "!")
+
+# boolean variable  
+is_student = True
+is_teacher = False
+
+# integer variable
+age = 20
+print(f"{name} is {age} years old.")
+
+# float variable
+height = 5.6
+print(f"{name} is {height} feet tall.")
