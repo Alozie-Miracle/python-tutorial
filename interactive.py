afcf336d6b9cacf8afdba8ce53776c1b12f87efe
Alozie Miracle Patrick <nacotech09@gmail.com>
2026-07-21 17:12:50 +0100

    added numeric expression

--- interactive.py
+++ interactive.py
@@ -1,14 +1,21 @@
-greeting = "Hello"
+# greeting = "Hello"
 # name = "Joe"
-question = "How are you?"
+# question = "How are you?"
 
 # # different ways to print the greeting, name, and question
 # print(greeting + ", " + name + "! " + question)
 # print(greeting, name, question)
 
 # making thus interactive by asking for user input
-name = input("What is your name? ")
+# name = input("What is your name? ")
 # print("What's your name? ")
 # or name = input()
 
-print(greeting + ", " + name + "! " + question)
+# print(greeting + ", " + name + "! " + question)
+
+# Numeric variables
+width = 15
+length = 20
+
+area = width * length
+print("The area of the rectangle is:", area)
