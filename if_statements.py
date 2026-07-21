600a500f1eed34d4e3f39e5153a1189e356cdec2
Alozie Miracle Patrick <nacotech09@gmail.com>
2026-07-21 18:09:53 +0100

    added if statement

---
+++ if_statements.py
@@ -0,0 +1,48 @@
+# if statements
+
+# name = input("Enter your name: ")
+
+# age = int(input("Enter your age: "))
+
+# if age < 18:
+#     print(f"Sorry {name}, you are not old enough to vote.")
+
+
+# print(f"Hello {name}, you are {age} years old.")
+
+# else statement
+# if age < 18:
+#     print(f"Sorry {name}, you are not old enough to vote.")
+# else:
+#     print(f"Hello {name}, you are old enough to vote.")
+
+
+# constants
+# VOTING_AGE = 18
+# name = input("Enter your name: ")
+
+# age = int(input("Enter your age: "))
+
+# if age < VOTING_AGE:
+#     print(f"Sorry {name}, you are not old enough to vote.")
+# else:
+#     print(f"Hello {name}, you are old enough to vote.")
+
+
+# # if else if statements
+# if age < VOTING_AGE:
+#     print(f"Sorry {name}, you are not old enough to vote.")
+# elif age == VOTING_AGE:
+#     print(f"Hello {name}, you are exactly old enough to vote.")
+# else:
+#     print(f"Hello {name}, you are old enough to vote.")
+
+
+# more comperison operators
+# ==, <, >, !=, <=, >=
+print("cat" == "cat")  # True
+print("cat" != "dog")  # True
+print(5 < 10)  # True
+print(5 > 10)  # False
+print(5 <= 10)  # True
+print(5 >= 10)  # False
