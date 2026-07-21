f6d97d7332dfa1d3a3be6066a67d4ad207235cc4
Alozie Miracle Patrick <nacotech09@gmail.com>
2026-07-21 16:49:16 +0100

    made it interactive

---
+++ interactive.py
@@ -0,0 +1,14 @@
+greeting = "Hello"
+# name = "Joe"
+question = "How are you?"
+
+# # different ways to print the greeting, name, and question
+# print(greeting + ", " + name + "! " + question)
+# print(greeting, name, question)
+
+# making thus interactive by asking for user input
+name = input("What is your name? ")
+# print("What's your name? ")
+# or name = input()
+
+print(greeting + ", " + name + "! " + question)
